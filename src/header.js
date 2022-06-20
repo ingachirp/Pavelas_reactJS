07a760deaa0bfaa2447dcbdd038ab7be3c9ee11c
I.G <ingrida.gudaityte@gmail.com>
2022-06-20 20:07:51 +0300

    dadeta eulute, skaiciuojanti bendra click'u skaiciu

--- src/header.js
+++ src/header.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 export default function Header (props) {
     const { title, subTitle } = props;
     const [count, setCount] = useState(0);
+    const [buttonPressed, setButtonPressed] = useState(0);
     return (
         <div>
             <button
             onClick={() => {
                 setCount(count + 1);
+                setButtonPressed(buttonPressed + 1);
             }}
             >
                 +
@@ -15,12 +17,14 @@ export default function Header (props) {
             <button
             onClick={() => {
                 setCount(count - 1 );
+                setButtonPressed(buttonPressed + 1);
             }}
             >
                 -
             </button>
             <h1>{title}</h1>
             <h4>{count}</h4>
+            <b> Total button pressed:{buttonPressed}</b>
         </div>
       
     );
